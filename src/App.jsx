eb4af68c6267c5966c7b9b60c466e1f095a14aa1
ciliegia-dischi <ciliegiarecordss@outlook.com>
2025-09-23 22:20:47 +0200

    modern ui version

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
-// import Nav from './sections/Nav';
-// import Footer from './sections/Footer';
-import Home from './sections/Home';
-import Music from './sections/Music';
-import Gallery from './sections/Gallery';
-import Events from './sections/Events';
-import Contacts from './sections/Contacts';
-import Support from './sections/Support';
+import Nav from './sections/Nav';
+import Content from './sections/Content';
 
-const Main = () => {
-  return (
-    <main>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='home' element={<Home/>} />
-        <Route path='musica' element={<Music/>} />
-        <Route path='galleria' element={<Gallery/>} />
-        <Route path='eventi' element={<Events/>} />
-        <Route path='contatti' element={<Contacts/>} />
-      </Routes>
-      <Support />
-    </main>
-  );
-}
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      {/*<Nav />*/}
-      <Main />
-      {/*<Footer />*/}
+      <main>
+        <Nav />
+        <Content />
+      </main>
     </div>
   );
 }
-export default App;
